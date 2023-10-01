88692e6f15ffe17e455acc83a6506b83b96f1103
yobadagne <45487708+yobadagne@users.noreply.github.com>
2023-10-01 17:04:24 +0300

    new db

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,12 +1,17 @@
 const mysql  = require('mysql2');
 const dotenv = require("dotenv");
 dotenv.config();
+//const db = mysql.createPool({
+//    host: "localhost",
+ //   user: "root",
+  //  password : process.env.dbpassword,
+   // database : "sms"
+//});
 const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
+    host: "sql9.freesqldatabase.com",
+    user: "sql9650109",
     password : process.env.dbpassword,
-    database : "sms"
+    database : "sql9650109"
 });
-
 
 module.exports = db;
